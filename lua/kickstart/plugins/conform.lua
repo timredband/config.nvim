5591691cd05a6e407e31acce4e8bd286e4b7af92
Tim Redband <tim.redband@dunnhumby.com>
2024-04-08 14:28:01 -0400

    pull in changes

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,6 +1,16 @@
 return { -- Autoformat
   'stevearc/conform.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
+  lazy = false,
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
   opts = {
     notify_on_error = false,
     format_on_save = {
