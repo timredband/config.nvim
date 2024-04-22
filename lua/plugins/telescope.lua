d39b9ebf1f65a5d5589bb00c71a9c921c0fab6c8
Tim Redband <tim.redband@dunnhumby.com>
2024-04-22 13:38:26 -0400

    remove italics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -73,7 +73,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = '[S]earch git files' })
+    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search git files' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
