b47330b8673d69f56d9133d2410287099fdd0b36
Tim Redband <tim.redband@dunnhumby.com>
2024-08-15 10:33:46 -0400

    add to ensure installed list

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -219,10 +219,21 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'bashls',
       'black',
+      'dockerls',
+      'eslint',
+      'gopls',
+      'jsonls',
+      'lua_ls',
+      'marksman',
       'prettier',
+      'pyright',
       'shfmt',
       'stylua',
+      'terraformls',
+      'tsserver',
+      'yamlls',
     })
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
