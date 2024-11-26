808a832a69c60724467a8fcfef7d38d10bad8e90
Tim Redband <tim.redband@dunnhumby.com>
2024-11-26 12:09:41 -0500

    center cursor when returning to file

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
   group = vim.api.nvim_create_augroup('custom-cursor-remember', { clear = true }),
   desc = 'return cursor to where it was last time closing the file',
   pattern = '*',
-  command = 'silent! normal! g`"zv',
+  command = 'silent! normal! g`"zvzz',
 })
 
 -- [[ Basic Autocommands ]]
