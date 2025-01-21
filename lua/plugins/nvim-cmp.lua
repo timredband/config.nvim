ec17ce2357b242c01609509c863fb693fd64af86
timredband <timredband@gmail.com>
2025-01-21 10:37:15 -0500

    add insert mode movement keymaps

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -70,11 +70,29 @@ return { -- Autocompletion
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
+          else
+            local win = vim.api.nvim_get_current_win()
+            local pos = vim.api.nvim_win_get_cursor(win)
+
+            local current_line = vim.api.nvim_get_current_line()
+
+            if pos[2] ~= #current_line then
+              pos[2] = pos[2] + 1
+              vim.api.nvim_win_set_cursor(win, pos)
+            end
           end
         end, { 'i', 's' }),
         ['<C-h>'] = cmp.mapping(function()
           if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
+          else
+            local win = vim.api.nvim_get_current_win()
+            local pos = vim.api.nvim_win_get_cursor(win)
+
+            if pos[2] ~= 0 then
+              pos[2] = pos[2] - 1
+              vim.api.nvim_win_set_cursor(win, pos)
+            end
           end
         end, { 'i', 's' }),
       },
