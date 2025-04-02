7942eb12cd543e4e97f245efadc8833b70c1b218
timredband <timredband@gmail.com>
2025-04-02 14:50:35 -0400

    fix: qf go to first only on new qf

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,4 +1,8 @@
---- automatically go to first entry in quickfix on open
+--- automatically go to first entry if new quickfix
 vim.schedule(function()
-  vim.cmd [[cfirst]]
+  local pos = vim.api.nvim_win_get_cursor(0)
+
+  if pos[1] == 1 then
+    vim.cmd [[cfirst]]
+  end
 end)
