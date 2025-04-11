333d41a9afc771e25d8d31faffd298879c354528
timredband <timredband@gmail.com>
2025-04-11 11:13:33 -0400

    feat: update packages

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,16 +1,15 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    }
-  end,
+  opts = {
+    delay = 0,
+    icons = {
+      mappings = vim.g.have_nerd_font,
+      keys = {},
+    },
+    spec = {
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>t', group = '[T]oggle' },
+    },
+  },
 }
