fb5d9fd7907f7cb1356d40d800040f2a43ba0655
timredband <timredband@gmail.com>
2025-04-21 11:42:25 -0400

    fix: markdown preview opening

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,17 @@
+vim.cmd [[
+  function OpenMarkdownPreview (url)
+    let localhost_url = substitute(a:url, "^.*:", "http://localhost:", "")
+    execute "silent ! ~/links/chrome.exe --new-window " . l:localhost_url
+  endfunction
+]]
+
 return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   build = 'cd app && yarn install',
   init = function()
     vim.g.mkdp_filetypes = { 'markdown' }
+    vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
   end,
   ft = { 'markdown' },
 }
