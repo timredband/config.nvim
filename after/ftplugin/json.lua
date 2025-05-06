b900d315f831c159a795de74c0fa1def3a4a24f6
timredband <timredband@gmail.com>
2025-05-06 11:01:46 -0400

    fix: package.json config

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,4 +1,4 @@
-if vim.fn.expand '%' == 'package.json' then
+if vim.fn.expand '%:t' == 'package.json' then
   vim.keymap.set('n', 'gx', function()
     vim.cmd [[normal "yyiq]]
     vim.ui.open('https://www.npmjs.com/package/' .. vim.fn.getreg 'y')
