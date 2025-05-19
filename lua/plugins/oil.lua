9ce69fbf235229e40a58914433822d5e73e88d56
timredband <timredband@gmail.com>
2025-05-19 10:52:02 -0400

    feat(oil): try oil
    Try oil instead of mini.files

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        return name == '.git'
+      end,
+    },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = false,
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Open Oil' },
+  },
+}
