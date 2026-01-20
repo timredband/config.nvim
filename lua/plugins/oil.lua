638eb005aa2f46fda11a167d1bd54c6fc949b8ed
timredband <timredband@gmail.com>
2026-01-20 17:04:14 -0500

    feat(oil): use <BS> to open/close

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -20,6 +20,6 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
   keys = {
-    { '-', '<cmd>Oil<cr>', desc = 'Open Oil' },
+    { '<BS>', '<cmd>Oil<cr>', desc = 'Open Oil' },
   },
 }
