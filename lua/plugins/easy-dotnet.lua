015441274e5e20523ab21b39f7e5f676f8952773
Tim Redband <timredband@gmail.com>
2026-07-30 15:17:54 -0400

    Disable easy-dotnet plugin setup

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,7 +1,8 @@
-return {
-  'GustavEikaas/easy-dotnet.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-  config = function()
-    require('easy-dotnet').setup()
-  end,
-}
+return {}
+-- return {
+--   'GustavEikaas/easy-dotnet.nvim',
+--   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+--   config = function()
+--     require('easy-dotnet').setup()
+--   end,
+-- }
